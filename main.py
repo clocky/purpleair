7b151d8a1bc69f33025bb47b342a196eee006b3c
Mark McLaughlin <mark@mclaughlin.me.uk>
2021-06-27 16:49:11 +0000

    Use AQI package to summarise

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 from purpleair.sensor import Sensor
-channel = Sensor(102160).parent
+import aqi
+import colored
 
-c = f"{round(channel.current_temp_c, 1)}\u00b0C"
-f = f"{channel.current_temp_f}\u00b0F"
-h = f"{channel.current_humidity}%"
+SENSOR_ID = 102160
 
-print(f"It's {f} ({c}) with {h} humidity.")
+
+def main():
+    channel = Sensor(SENSOR_ID).parent
+    c = f"{round(channel.current_temp_c, 1)}\u00b0C"
+    f = f"{channel.current_temp_f}\u00b0F"
+    h = f"{channel.current_humidity}%"
+    q = aqi.to_aqi([(aqi.POLLUTANT_PM25, channel.current_pm2_5)])
+    d = aqi_to_category(q)
+
+    print(f"It's {f} ({c}) with {h} humidity, with {d} AQI of {q}.")
+
+
+def aqi_to_category(index):
+    if index >= 0 and index <= 50:
+        description = colored.stylize("a good", "green")
+    elif index >= 51 and index <= 100:
+        description = colored.stylize("a moderate", colored.bg("yellow"))
+    elif index >= 101 and index <= 150:
+        description = "an unhealthy for sensitive groups"
+    elif index >= 151 and index <= 200:
+        description = "an unhealthy"
+    elif index >= 201 and index <= 300:
+        description = "a very unhealthy"
+    elif index >= 301 and index <= 500:
+        description = "a hazardous"
+
+    return description
+
+
+if __name__ == "__main__":
+    main()
